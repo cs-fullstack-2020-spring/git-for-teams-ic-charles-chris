934a3c2e70fcfb531c524abe85ec65678bef1c2b
chris huntley <cjhuntley4@gmail.com>
2020-03-05 11:33:15 -0600

    second merge

--- Script.js
+++ Script.js
@@ -1,2 +1,15 @@
 let section_A= document.getElementById('sectionA')
-let firstButton=document.getElementById('')
+let section_B= document.getElementById('sectionB')
+let section_C= document.getElementById('sectionC')
+let section_D= document.getElementById('sectionD')
+
+let firstButton=document.querySelector('.firstButton')
+let firstButton=document.querySelector('.scondButton')
+let firstButton=document.querySelector('.thirdButton')
+
+
+
+firstButton.addEventListener('click', onClick)
+function onClick(event){
+
+}section_A.innerHTML=${}
